c6341302ea4997d8c44c0fe9f3915be82bdaa859
Sergey Pastukhov <sergey.pastukhov@booking.com>
2017-07-16 15:00:51 +0200

    Minor refactoring

--- RL/training_methods.py
+++ RL/training_methods.py
@@ -1,47 +1,34 @@
 import numpy as np
 from scipy import stats
 
-# TODO: Fix
-def train_discounted(env, agent, params):
-    """Pass discounted reward to the agent"""
+def train_discounted_rewards(env, agent, params):
     rewards = []
     
-    all_replays = []
-    
     for episode in range(params.episodes_count):
-        state = np.reshape(env.reset(), [1, params.state_size])        
-        frame = 0
+        state = env.reset()
+        total_reward = 0.0
         
-        states = []
         replays = []
         
         for frame in range(params.max_frame_in_episode):
             action = agent.act(state, frame)
-            next_state, _, done, _ = env.step(action)
-            next_state = np.reshape(next_state, [1, params.state_size])
+            next_state, reward, done, _ = env.step(action)
+
+            total_reward += reward
     
             if done:
                 break
 
-            states.append(state[0])
-            replays.append((state, action, next_state))
+            replays.append((frame, state, action, reward, next_state))
             state = next_state
-        
-#         states = np.array(states)
-#         predicted = agent.model.predict(states)
-#         reward_value = 0.0
-#         for index in reversed(range(len(replays))):
-#             state, action, next_state = replays[index]
-#             reward_value = reward_value * params.gamma + predicted[index, action]
-#             agent.remember(state, action, reward_value, next_state)
-        
-        for frame, replay in enumerate(replays):
-            state, action, next_state = replay
-            reward_value = len(replays) - frame
-            agent.remember(state, action, reward_value, next_state)
-            
-        rewards.append(len(replays))
-            
+
+        rewards.append(total_reward)
+
+        discounted_reward = 0.0
+        for frame, state, action, reward, next_state in reversed(replays):
+            discounted_reward = reward + discounted_reward * params.gamma
+            agent.remember(state, action, discounted_reward, next_state, frame)
+
         if (episode + 1) % max(1, (params.episodes_count / 20)) == 0:
             print("episode: {}/{}, reward {}, exploration rate: {:.2}"
               .format(episode + 1, params.episodes_count, np.mean(rewards[-10:]), params.epsilon))
@@ -57,33 +44,27 @@ def train_reward_is_time(env, agent, params):
     """Ignore reward from the env, agent will be trained to increase total time played"""
     rewards = []
     
-    all_replays = []
-    
     for episode in range(params.episodes_count):
-        state = np.reshape(env.reset(), [1, params.state_size])        
-        frame = 0
+        state = env.reset()
         
-        states = []
         replays = []
         
         for frame in range(params.max_frame_in_episode):
             action = agent.act(state, frame)
             next_state, _, done, _ = env.step(action)
-            next_state = np.reshape(next_state, [1, params.state_size])
-    
+
             if done:
                 break
 
-            states.append(state[0])
             replays.append((state, action, next_state))
             state = next_state
+            
+        rewards.append(len(replays))
         
         for frame, replay in enumerate(replays):
             state, action, next_state = replay
             reward_value = len(replays) - frame
             agent.remember(state, action, reward_value, next_state, frame)
-            
-        rewards.append(len(replays))
             
         if (episode + 1) % max(1, (params.episodes_count / 20)) == 0:
             print("episode: {}/{}, reward {}, exploration rate: {:.2}"
@@ -100,14 +81,12 @@ def train(env, agent, params):
     rewards = []
     
     for episode in range(params.episodes_count):
-        state = np.reshape(env.reset(), [1, params.state_size])  
-
+        state = env.reset()
         total_reward = 0.0
         
         for frame in range(params.max_frame_in_episode):
             action = agent.act(state, frame)
             next_state, reward, done, _ = env.step(action)
-            next_state = np.reshape(next_state, [1, params.state_size])
     
             total_reward += reward
     
